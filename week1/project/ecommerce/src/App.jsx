5fcf26242f9c3e596753667a4d7c5da774e2a92d
ervandogan12 <ervandogan12@gmail.com>
2024-06-05 11:50:47 +0200

    ahmet_dogan react week1 resolved ecommerce

--- week1/project/ecommerce/src/App.jsx
+++ week1/project/ecommerce/src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
 import products from './fake-data/all-products';
 import categories from './fake-data/all-categories';
-import { AllCategories } from './components/all_categories';
-import { AllProducts } from './components/all_products';
+import { AllCategories } from './components/Categories';
+import { AllProducts } from './components/Products';
 import './App.css';
 
 function App() {
   const [categoryState, setCategoryState] = useState();
   const categoryProducts = categoryState ? products.filter(product => product.category.includes(categoryState.split(':')[1].trim())) : products;
+  
+  const handleCategoryClick = (category) => {
+
+    if (categoryState === category) {
+      setCategoryState(undefined); 
+    } else {
+      setCategoryState(category);
+    }
+  };
 
   return (
     <div className="ecommerce-aplication">
       <h1>Products</h1>
-      <AllCategories categories={categories} setCategoryState={setCategoryState} categoryState={categoryState} />
+      <AllCategories categories={categories} setCategoryState={handleCategoryClick} categoryState={categoryState} />
       <AllProducts products={categoryProducts} />
     </div>
   );
--- week1/project/ecommerce/src/components/all_categories.jsx
+++
@@ -1,9 +0,0 @@
-export const AllCategories = ({ categories, setCategoryState, categoryState }) => (
-    <div className="categories">
-        {categories.map(category => (
-          <div key={category} onClick={() => setCategoryState(category)} className={categoryState === category ? 'category-item selected-category-item' : 'category-item'}>
-            {category}
-          </div>
-        ))}
-    </div>
-  );
